4d51bb533a8c46fb827baf00fc898786385de180
harsh <harshpandey9324@gmail.com~>
2023-02-03 01:47:08 +0530

    Form seekha|| Textfield Validation Seekha|| Ink seekha==> Wapis revise krna yeh

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -8,103 +8,126 @@ class loginPage extends StatefulWidget {
 class _loginPageState extends State<loginPage> {
   String name = "";
   bool changeButton = false;
+
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, "/home");
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
           //SingleChildScrollView is used to scroll the page to avoid overflow error
-          child: Column(
-            children: [
-              SizedBox(
-                height: 40.0,
-              ),
-              Image.asset(
-                "assets/images/peterbhau.png",
-                fit: BoxFit.cover,
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "Mr Peter welcomes $name",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32),
-                child: Column(
-                  children: [
-                    TextFormField(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 90.0,
+                ),
+                Text(
+                  "The Family Guy",
+                  style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 15, 15, 16)),
+                ),
+                Image.asset(
+                  "assets/images/peterbhau.png",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Welcome Dear $name  :Peter",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 32),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Enter Username",
+                              labelText: "Username"),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Username cannot be empty";
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            name = value;
+                            setState(
+                                () {}); //setState is used beause we are changing the state of the widget so we need to call setState to change the state
+                          }),
+                      TextFormField(
+                        obscureText: true,
                         decoration: InputDecoration(
-                            hintText: "Enter Username", labelText: "Username"),
-                        onChanged: (value) {
-                          name = value;
-                          setState(
-                              () {}); //setState is used beause we are changing the state of the widget so we need to call setState to change the state
-                        }),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: "Enter Password", labelText: "Password"),
-                    ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
-
-                    InkWell(
-                      onTap: () async {
-                        setState(() {
-                          changeButton = true;
-                        });
-                        await Future.delayed(Duration(seconds: 1));
-                        Navigator.pushNamed(context, "/home");
-                      },
-                      child: AnimatedContainer(
-                        duration: Duration(seconds: 1),
-                        width: changeButton ? 50 : 150,
-                        height: 40,
-                        alignment: Alignment.center,
-                        child: changeButton
-                            ? Icon(
-                                Icons.done,
-                                color: Colors.white,
-                              )
-                            : Text(
-                                'Login',
-                                style: TextStyle(
+                            hintText: "Enter Password", labelText: "Password"),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password cannot be empty";
+                          } else if (value.length < 6) {
+                            return "Password length should be atleast 6";
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      Material(
+                        color: Color.fromARGB(255, 44, 106, 47),
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 20),
+                        child: InkWell(
+                          onTap: () => moveToHome(context),
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            width: changeButton ? 50 : 150,
+                            height: 40,
+                            alignment: Alignment.center,
+                            child: changeButton
+                                ? Icon(
+                                    Icons.done,
                                     color: Colors.white,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 39, 125, 42),
-                          borderRadius:
-                              BorderRadius.circular(changeButton ? 50 : 20),
+                                  )
+                                : Text(
+                                    'Login',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                          ),
                         ),
                       ),
-                    ),
-                    // ElevatedButton(
-                    //   child: Text("Login"),
-                    //   style: TextButton.styleFrom(
-                    //     minimumSize: Size(150, 40),
-                    //     // style is used to change the style of button
-                    //     backgroundColor: Color.fromARGB(255, 44, 110, 38),
-                    //   ),
-                    //   onPressed: () {
-                    //     Navigator.pushNamed(context, "/home");
-                    //   },
-                    // ),
-                  ],
-                ),
-              )
-            ],
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
